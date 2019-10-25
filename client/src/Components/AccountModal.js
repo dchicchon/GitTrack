1e3fc0e2570ec3e615615a9cd6941505926d1c78
dchicchon <danielchicchon@gmail.com>
2019-10-25 15:39:23 -0700

    AccountModal broken currently due to formatting bug for some reason. Fix later

--- client/src/Components/AccountModal.js
+++ client/src/Components/AccountModal.js
@@ -2,38 +2,57 @@ import React from 'react';
 
 import Message from './Message';
 
+
+// This will have many tabs that will display depending on what I click
+
+function openCity(event, accountType) {
+    console.log(event.target)
+    console.log(accountType)
+}
+
 function AccountModal(props) {
+   
     return (
 
         <div className='container' style={{ width: '50%', border: '1px solid #1b1e22', background: '#202329', padding: '1rem' }}>
-            <div className='row'>
-                <h1>Create Account</h1>
-                <Message
-                    message={props.message}
-                    color={props.color}
-                />
-                <div className='close' onClick={props.close}>x</div>
+         <div className='row'>
+    <h1>Create Account</h1>
 
-            </div>
+    <Message
+        message={props.message}
+        color={props.color}
+    />
+        <div className='close' onClick={props.close}>x</div>
 
-            <form>
+            </div >
+
+
+    {/* Tab Links */ }
+    <div className='tab'>
+        <button className='tablink' onClick={openCity(event, 'administrator')}>Administrator</button>
+        <button className='tablink' onClick={openCity(event, 'instructor')}>Instructor</button>
+        <button className='tablink' onClick={openCity(event, 'admin')}>Student</button>
+
+    </div>
+
+    {/* Tab Content */ }
+
+    {/* Form should mostly stay the same? Except for students */ }
+    {/* <form>
                 <div className='row'>
                     <div className="form-group col-6">
                         <label htmlFor="firstName">First Name</label>
                         <input value={props.firstName} name='firstName' onChange={props.handleInputChange} type="text" className="form-control" id="firstName" placeholder="First Name" />
-                        {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
                     </div>
                     <div className="form-group col-6">
                         <label htmlFor="lastName">Last Name</label>
                         <input value={props.lastName} name='lastName' onChange={props.handleInputChange} type="text" className="form-control" id="lastName" placeholder="Last Name" />
-                        {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
                     </div>
                 </div>
                 <div className='row'>
                     <div className="form-group col-12">
                         <label htmlFor="email">Email address</label>
                         <input value={props.email} name='email' onChange={props.handleInputChange} type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
-                        {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
                     </div>
                 </div>
                 <div className='row'>
@@ -48,31 +67,12 @@ function AccountModal(props) {
                         <input value={props.passwordConfirm} name='passwordConfirm' onChange={props.handleInputChange} type="password" className="form-control" id="passwordConfirm" placeholder="Password" autoComplete="true" />
                     </div>
                 </div>
-                {/* <fieldset className="form-group types">
-                    <div className="row">
-                        <legend className="col-form-label col-sm-2 pt-0">Account Type</legend>
-                        <div className="col-sm-10">
-                            <div className="form-check">
-                                <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="admin" />
-                                <label className="form-check-label" htmlFor="gridRadios1">Administrator</label>
-                            </div>
-                            <div className="form-check">
-                                <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="instructor" />
-                                <label className="form-check-label" htmlFor="gridRadios2">Instructor</label>
-                            </div>
-                            <div className="form-check">
-                                <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="student" />
-                                <label className="form-check-label" htmlFor="gridRadios3">Student</label>
-                            </div>
-                        </div>
-                    </div>
-                </fieldset> */}
                 <div className='row'>
                     <button onClick={props.accountCreate} type="submit" className="btn btn-primary col-4 center-signup">Submit</button>
                 </div>
-            </form>
+            </form> */}
 
-        </div>
+        </div >
     )
 }
 
